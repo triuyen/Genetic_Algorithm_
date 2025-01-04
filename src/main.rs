8666a239636ad1e67cb905dabda8c5c85fec1088
TRIUYEN_TANG <triuyentang@gmail.com>
2025-01-04 12:54:19 +0100

    add main file back

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use bevy::prelude::*;
+use simulation::InitPlugin;
+use world::WorldPlugin;
+use bevy_egui::{egui, EguiContexts, EguiPlugin};
+use bevy_fly_cam::FlyCamPlugin;
+
+use bevy_debug_grid::*;
+
+mod simulation;
+mod world;
+
+fn main() {
+    App::new()
+        .add_plugins((DefaultPlugins, 
+                        WorldPlugin,           
+                        InitPlugin,
+                        FlyCamPlugin,
+                        DebugGridPlugin::with_floor_grid()))
+        .add_plugins(EguiPlugin)
+        .add_systems(Update, e_gui)
+        .run();
+}
+
+fn e_gui(
+    mut contexts: EguiContexts,
+    commands: Commands,
+    materials: ResMut<Assets<StandardMaterial>>,
+    query: Query<(Entity, &Handle<StandardMaterial>, Option<&Parent>, Option<&Children>)>,
+    ) {
+    let value = simulation::evaluate_fitness(commands, query, materials);
+    egui::Window::new("Example Window").show(contexts.ctx_mut(), |ui| {
+        if ui.button("Run_simulation").clicked() {
+            value;
+        }
+    });
+}
